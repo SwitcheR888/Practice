a4784fb40676ff5c147ad39f316332f1eb3250a2
SwitcheR <19fkf86@gmail.com>
2026-08-19 23:57:33 +0300

    Changed the conditions for the next task

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const conditions = 'agreement';
 
 const message = `${nameMentor} is my teacher. His duties are ${service} HTML, CSS, JavaScript, etc. We have ${conditions} with him.`;
 console.log(message);
+
+const studentName = 'Kyryl';
+const lastName = 'Fedorovskyi';
+
+const initials = `${studentName[0]}.${lastName[0]}.`;
+console.log(initials);
+
+const lengthMessage = `${studentName} has ${studentName.length} letters`;
+console.log(lengthMessage);
