8d6eeec4e7646b6590442ec9aef36bee09e9737b
SwitcheR <19fkf86@gmail.com>
2026-08-20 20:28:16 +0300

    tasks with equality operators

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 'use strict';
-const nameMentor = 'G';
-const service = 'teach me';
-const conditions = 'agreement';
 
-const message = `${nameMentor} is my teacher. His duties are ${service} HTML, CSS, JavaScript, etc. We have ${conditions} with him.`;
-console.log(message);
+const bookedSlots = 8;
+const maxSlots = 8;
+const appointmentStatus = 'confirmed';
 
-const studentName = 'Kyryl';
-const lastName = 'Fedorovskyi';
+const isFull = bookedSlots === maxSlots;
+console.log('isFull', isFull);
 
-const initials = `${studentName[0]}.${lastName[0]}.`;
-console.log(initials);
-
-const lengthMessage = `${studentName} has ${studentName.length} letters`;
-console.log(lengthMessage);
+const needsConfirmation = appointmentStatus !== 'confirmed';
+console.log('needsConfirmation', needsConfirmation);
