b56bc34de8437ab487996c750e704c03b3845ff3
SwitcheR <19fkf86@gmail.com>
2026-09-23 00:43:36 +0300

    A new topic has been added

---
+++ oop-classes.js
@@ -0,0 +1,20 @@
+'use strict';
+//! ===== КЛЮЧЕВОЕ СЛОВО this =====
+/*
+const user = {
+  name: 'Кирилл', //! user.name - св-во объекта. Получаем: Кирилл
+
+  greet() {     //! greet - метод объекта
+    console.log(this.name); //! this.name означает: «Возьми свойство name у объекта greet
+  },
+};
+
+user.greet(); //! вызываем метод объекта
+*/
+/*
+function greet(name) {
+  console.log(name);
+}
+
+greet('Кирилл');
+*/
